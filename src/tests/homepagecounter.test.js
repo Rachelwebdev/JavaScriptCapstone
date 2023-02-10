dd15303ba2ba305924287e8dc5e3e14a8c8b0676
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-10 23:14:39 +0400

    Delete commented lines of codes

--- src/tests/homepagecounter.test.js
+++ src/tests/homepagecounter.test.js
@@ -1,25 +1,5 @@
 import homePageCount from '../modules/homePageCounter.js';
 
-// const home = document.createElement("ul");
-// home.classList.add("movie-section");
-// for (let i = 1; i < 7; i += 1) {
-//   const div = document.createElement("div");
-//   home.appendChild(div);
-// }
-
-// describe([home, "(6)"])(
-//   "check number of shows in a page: ",
-//   (mainPage, expected) => {
-//     document.body.innerHTML = `<span class="show-count"></span>`;
-//     document.body.appendChild(mainPage);
-//     const count = document.body.querySelector(".show-count");
-//     homePageCount(count);
-//     test(`test with ${mainPage.childElementCount} number of items in the page:`, () => {
-//       expect(count.innerHTML).toBe(expected);
-//     });
-//   }
-// );
-
 describe('test for movie: ', () => {
   test('6 movies Added', () => {
     document.body.innerHTML = `
